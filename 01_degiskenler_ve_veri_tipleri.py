efa634c3798482d652046a9f755fd5718f2c0a11
morvampir5 <numanmak48@icloud.com>
2023-03-04 13:01:32 +0300

    degisken

---
+++ 01_degiskenler_ve_veri_tipleri.py
@@ -0,0 +1,24 @@
+# Değişken tanımlama örnekleri
+x = 5
+y = "Hello World"
+z = True
+
+
+# VERİ TİPLERİ
+# 1. Tam sayılar (integer)
+x = 15
+y = -15
+z = 0
+
+
+# 2 ondalıklı sayılar (floats)
+x = 3.14
+y = -2.31
+z = 0.5
+
+
+# 3 Metin (string)
+selam = "merhaba"
+ad = "esek"
+mesaj = selam + " " + ad
+print(mesaj)
